1611dfa735ac69ff22626bc67ac7a9bf83252c99
kody <w.jamesdevera@gmail.com>
2025-05-29 20:30:23 +0800

    add links

---
+++ lib/links.ts
@@ -0,0 +1,23 @@
+type Link = {
+  label: string;
+  url: string;
+};
+
+export const links: Link[] = [
+  {
+    label: "Dashboard",
+    url: "/",
+  },
+  {
+    label: "Attendance",
+    url: "/attendance",
+  },
+  {
+    label: "Kids",
+    url: "/kids",
+  },
+  {
+    label: "Services",
+    url: "/services",
+  },
+];
