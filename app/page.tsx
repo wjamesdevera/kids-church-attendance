d4e8d5498bd7050588b8fea58a4c2c2a99ba48f7
w.jamesdevera <w.jamesdevera@gmail.com>
2025-06-23 12:15:49 +0800

    refactor: remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import AuthProvider from "@/components/auth/auth-provider";
 import ProtectedRoute from "@/components/auth/protected-route";
 import SignOutButton from "@/components/auth/sign-out-button";
 import SidebarLayout from "@/components/layouts/sidebar-layout";
-import Loader from "@/components/loader";
-import Test from "@/components/test";
 
 export default function Home() {
   return (
